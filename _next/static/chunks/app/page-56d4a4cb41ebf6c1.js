3cd6830d0a4ac6198f150a1948c1ede00e83d9bb
easylogic <easylogic@users.noreply.github.com>
2024-10-23 17:57:57 +0000

    deploy: 4c11a4c90e1cd864708bbb48b69cc9fada13443c

---
+++ _next/static/chunks/app/page-56d4a4cb41ebf6c1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{52819:function(n,e,u){Promise.resolve().then(u.t.bind(u,10061,23))}},function(n){n.O(0,[61,738,720,744],function(){return n(n.s=52819)}),_N_E=n.O()}]);
